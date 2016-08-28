0a2bb22114d096a0fc47a7e1f7fd076846c568fd
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-08-28 21:31:06 +0200

    putSavedTasks method in ToDoService

--- app/src/main/java/com/nekodev/paulina/sadowska/todolist_mvvm/data/remote/ToDoService.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/todolist_mvvm/data/remote/ToDoService.java
@@ -4,7 +4,12 @@ import com.nekodev.paulina.sadowska.todolist_mvvm.model.ToDoItem;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import rx.Observable;
 
 /**
@@ -18,5 +23,13 @@ public interface ToDoService {
 
     @GET("/todo_list/")
     Observable<List<ToDoItem>> getTasks();
+
+
+    @FormUrlEncoded
+    @PUT("/todo_list/{id}")
+    Observable<ResponseBody> putSavedTasks(@Field("title") String title,
+                                     @Field("userId") int userId,
+                                     @Path("id") int id,
+                                     @Field("completed") boolean completed);
 
 }
