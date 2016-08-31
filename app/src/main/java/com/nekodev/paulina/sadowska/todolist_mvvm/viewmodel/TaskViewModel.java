cc369cb901a5af1dd642377c4f99b1202130b6a5
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-08-31 18:34:43 +0200

    change text color of completed task

--- app/src/main/java/com/nekodev/paulina/sadowska/todolist_mvvm/viewmodel/TaskViewModel.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/todolist_mvvm/viewmodel/TaskViewModel.java
@@ -3,8 +3,13 @@ package com.nekodev.paulina.sadowska.todolist_mvvm.viewmodel;
 import android.app.Activity;
 import android.content.Context;
 import android.databinding.BaseObservable;
+import android.databinding.BindingConversion;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
+import com.nekodev.paulina.sadowska.todolist_mvvm.R;
 import com.nekodev.paulina.sadowska.todolist_mvvm.controller.activity.EditTaskActivity;
 import com.nekodev.paulina.sadowska.todolist_mvvm.model.ToDoItem;
 
@@ -37,6 +42,18 @@ public class TaskViewModel extends BaseObservable {
 
     public void setTask(String taskText){
         mTask.setTask(taskText);
+    }
+
+    public int getTaskTextColor(){
+        if(mTask.isCompleted()){
+            return ContextCompat.getColor(mContext, R.color.secondaryTextColor);
+        }
+        return ContextCompat.getColor(mContext, R.color.primaryTextColor);
+    }
+
+    @BindingConversion
+    public static ColorDrawable convertColorToDrawable(String color) {
+        return new ColorDrawable(Color.parseColor(color));
     }
 
     public String getId(){  return mTask.getId() + ""; }
